89e43e963770e33855c008a1a3ae85722c502635
PavaniVattikolla <147476015+PavaniVattikolla@users.noreply.github.com>
2026-02-09 19:32:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,22 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
 dotenv.config();
-
 const app = express();
-
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-
-const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send("Multi-Tenant SaaS Backend Running 🚀");
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "UP", message: "Healthy" });
 });
-
+app.get("/", (req, res) => {
+  res.send("Backend Running");
+});
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
